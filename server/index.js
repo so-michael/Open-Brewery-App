86955fed4864294f7fec48112aada8a90c9ec72a
so-michael <michaelso321@gmail.com>
2022-04-22 10:08:30 -0400

    sercer changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 
-app.get('/', (req, res) => {
-    res.send("hello world")
+//create connection to database
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'openbrewery'
 })
+
+//test query by printing to console
+db.query("SELECT * FROM openbrewery.texasbreweries", (err, res) => {
+    return console.log(res)
+})
+
+//send to server
+app.get('/api/get', (req, res) => {
+    //const sqlSELECT = "SELECT * FROM texasbreweries";
+    db.query("SELECT * FROM texasbreweries", (err, result)=> {
+        res.send(result);
+    });
+});
+
 app.listen(3002, () => {
-    console.log
+    console.log("running on port 3002");
 })
